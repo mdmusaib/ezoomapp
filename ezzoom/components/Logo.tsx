68199a7e0c4aad74175e75b6054b99e07ebe2484
Melvisharam info <63548063+mvsweb@users.noreply.github.com>
2022-01-29 07:06:27 +0000

    hifzur

--- ezzoom/components/Logo.tsx
+++ ezzoom/components/Logo.tsx
@@ -9,7 +9,7 @@ export default function Logo({ small, icon }: { small?: boolean; icon?: boolean 
             className={small ? "h-4 w-auto" : "h-5 w-auto"}
             alt="Cal"
             title="Cal"
-            src="/calendso-logo-white-word.svg"
+            src="/logo.png"
           />
         )}
       </strong>
